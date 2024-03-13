72d5b28838ac194f88a9b1cbd9a755b6a6470017
Harivan-T <harivantahir@gmail.com>
2024-03-13 05:44:43 +0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+   Write the Big O time complexity of your function here
+   O(n^2)
+   n is the number of elements in the input array
+   and there are two nested loops resulting in n * (n-1) iterations
 */
 
 /* 
   Add your pseudocode here
+   for(i=0 in array){
+      for(j=i+1 in array){
+        if(array[j]+array[i]===targey)
+        return true;
+      }
+  }return false;
 */
 
 /*
   Add written explanation of your solution here
+   The hasTargetSum function takes an array and a target value as input, 
+  sorts the array, and then uses two pointers to find if there are two number
+  s that sum up to the target value. If such a pair exists, it returns true; otherwise, 
+  it returns false. The function's time complexity is O(n log n) due to the sorting operation, 
+  where n is the number of elements in the input array.
+  
 */
 
 // You can run `node index.js` to view these console logs
